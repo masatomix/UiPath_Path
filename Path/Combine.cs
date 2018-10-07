05715645b642c64545d8d5c6db1799064206cc46
Masatomi KINO <masatomix@ki-no.org>
2018-10-08 08:52:40 +0900

    初期状態

--- Path/Combine.cs
+++ Path/Combine.cs
@@ -12,12 +12,15 @@ namespace Path
         // 文字列型のアクティビティ入力引数を定義します
         public InArgument<string> Text { get; set; }
 
+        public OutArgument<String> result { get; set; }  // 追記
+
         // アクティビティが値を返す場合は、CodeActivity<TResult> から派生して、
         // Execute メソッドから値を返します。
         protected override void Execute(CodeActivityContext context)
         {
             // テキスト型の入力引数のランタイム値を取得します
             string text = context.GetValue(this.Text);
+            result.Set(context, text + " Hello world.");  // 追記
         }
     }
 }
